e45857b6d4461dfd81fb77f39c39c532bfbc824e
diegomrod <dmr95dmr@gmail.com>
2014-02-24 02:54:58 -0300

    24/02

--- controllers/public/MainController.js
+++ controllers/public/MainController.js
@@ -1,17 +1,13 @@
-(function () {
-  function MainController () {}
-  
-  MainController.prototype.data = {
-    "title" : "Home"
-  };
-  
-  MainController.prototype.Index = function (req, res) {
-    //var user_data = new Profile().getProfile(session.id); // Implementar
-    //var Timeline = new TimelineController()				// Implementar
-	//var posts = Timeline.buildFromList(session.user.id)	// Implementar
-	
-	res.render('public/feed.jade', this.data);
-  };
-  
-  module.exports = MainController;
+(function() {
+	function MainController() {}
+
+	MainController.prototype.Index = function(req, res) {
+		//var user_data = new Profile().getProfile(session.id); // Implementar
+		//var Timeline = new TimelineController()				// Implementar
+		//var posts = Timeline.buildFromList(session.user.id)	// Implementar
+		
+		res.render('public/feed.jade', {});
+	};
+
+	module.exports = MainController;
 }).call(this);
